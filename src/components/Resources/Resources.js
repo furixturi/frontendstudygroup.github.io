860bd8a1ded6e3b0a511859411581668424a3c3c
Xiaoli Shen <alabebop@gmail.com>
2022-04-08 09:56:30 +0900

    fetch resources dynamically from backend

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import '../../assets/css/App.css';
 import './Resources.css';
 import ResourcesList from '../data/data.json';
@@ -8,21 +8,48 @@ import Disclaimer from "../Disclaimer/Disclaimer";
 
 export default function Resources() {
 
-return (
-    <div className="container">
-        <Toggle />
-       <div className="grid">
-        {
-            ResourcesList.map(resource => 
-              <article key={resource.id}> 
-                <h1>{resource.name}</h1>
-                <p className="text">{resource.body}
-               </p>
-               <Link className="resourceLink" to={`/resources/${resource.id}`}><span>More info</span></Link>
+  /* 
+  Thank you Sunitha! 
+  For letting me borrow the code that fetches data dynamically!
+  https://github.com/sunithapatel/cloud-workshop-serverless/blob/main/ui/src/components/Resources/Resources.js
+  */
 
-              </article>)}
-      </div> 
+  const [resources, setResources] = useState([]);
+  // const apiURL = 'https://9hvgvee9e9.execute-api.ap-northeast-1.amazonaws.com/dev/resources'
+  const apiURL = ''
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiURL);
+        const json = await response.json();
+        const resources = JSON.parse(json.body)
+        setResources(resources);
+
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
+  return (
+    <div className="container">
+      <Toggle />
+      <div className="grid">
+        {
+          // ResourcesList.map(resource =>
+          resources.map(resource =>
+            <article key={resource.id}>
+              <h1>{resource.name}</h1>
+              <p className="text">{resource.body}
+              </p>
+              <Link className="resourceLink" to={`/resources/${resource.id}`}><span>More info</span></Link>
+
+            </article>)}
+      </div>
       <Disclaimer />
-   </div> 
+    </div>
   );
 }
